bbc3db821fd450d8e69a7702049ccbcaf5a75eb6
LuraMoth <fasreaperearnest@gmail.com>
2022-04-05 00:41:55 -0700

    integrate greeting system as a plugin

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
-use bevy::{prelude::{App, Commands, Query, With, Component}, DefaultPlugins};
+use bevy::{prelude::{App, Commands, Query, With, Component, Plugin}, DefaultPlugins};
 
 #[derive(Component)]
 struct Person;
 #[derive(Component)]
 struct Name(String);
 
+pub struct HelloPlugin;
 
-fn hello_world() {
-    println!("hello world!");
+impl Plugin for HelloPlugin {
+	fn build(&self, app: &mut App) {
+		app.add_startup_system(add_people)
+			.add_system(greet_people);
+	}
 }
 
+
 fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Kayla Mizuki".to_string()));
-    commands.spawn().insert(Person).insert(Name("Jacob Garrak".to_string()));
+	commands.spawn().insert(Person).insert(Name("Kayla Mizuki".to_string()));
+	commands.spawn().insert(Person).insert(Name("Jacob Garrak".to_string()));
 }
 
 fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
-    }
+	for name in query.iter() {
+		println!("hello {}!", name.0);
+	}
 }
-
 
 fn main() {
     App::new()
-		.add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
