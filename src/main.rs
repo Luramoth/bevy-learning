6af7a6f6d6718fc95a0b8d134bba2f346e924ddb
LuraMoth <fasreaperearnest@gmail.com>
2022-04-05 00:22:17 -0700

    start off with basic bevy features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::{prelude::{App, Commands, Query, With, Component}, DefaultPlugins};
+
+#[derive(Component)]
+struct Person;
+#[derive(Component)]
+struct Name(String);
+
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Kayla Mizuki".to_string()));
+    commands.spawn().insert(Person).insert(Name("Jacob Garrak".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
